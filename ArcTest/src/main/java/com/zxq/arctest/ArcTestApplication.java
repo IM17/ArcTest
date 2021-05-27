2d61e876d8f0b925c3468a727dddef7af600ef24
IM17 <409860217@qq.com>
2021-05-27 17:13:10 +0800

    第一次提交

---
+++ ArcTest/src/main/java/com/zxq/arctest/ArcTestApplication.java
@@ -0,0 +1,14 @@
+package com.zxq.arctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArcTestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ArcTestApplication.class, args);
+    }
+
+}
---
+++ ArcTest/src/test/java/com/zxq/arctest/ArcTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zxq.arctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArcTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
